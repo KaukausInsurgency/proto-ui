a8f141bff3cf92c3590d765770c88a7594e0bc68
Igneous01 <5571589+Igneous01@users.noreply.github.com>
2018-05-06 07:34:24 -0400

    Nav Collapsed on start, discord widget, player table
    basic content defined

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,6 +1,6 @@
 $(function() {
-	$('.dropdown-content').addClass('nav-expanded');	// by default the ribbon is nav-expanded
-	
+	$('.dropdown-content').addClass('nav-collapsed');	// by default the ribbon is collapsed
+	$('.js-nav-text').hide();	
 	$('.nav-expander').click(function()
 	{
 		var expander_icon = $('.js-expander-icon');
@@ -12,7 +12,7 @@ $(function() {
 				width: $('.prop-nav').css('--max-width').trim()
 			}, 200, function()
 			{
-				$('.nav-text').show();
+				$('.js-nav-text').show();
 			});
 			$('.dropdown-content').removeClass('nav-collapsed');
 			$('.dropdown-content').addClass('nav-expanded');
@@ -21,12 +21,34 @@ $(function() {
 		{
 			expander_icon.removeClass('fa-angle-double-left');
 			expander_icon.addClass('fa-angle-double-right');	
-			$('.nav-text').hide();		
+			$('.js-nav-text').hide();		
 			$('.sidenav').stop(true).animate({
 				width: $('.prop-nav').css('--min-width').trim()
 			}, 200);
 			$('.dropdown-content').removeClass('nav-expanded');			
 			$('.dropdown-content').addClass('nav-collapsed');
 		}		
-	});   
+	});  
+	
+	$('.server-dropdown').click(function()
+	{
+		var container = $(this.closest('.server-dropdown'));
+		container.toggleClass('js-flyout');
+		if (container.hasClass('js-flyout'))
+			container.children('.server-flyout-content').css('display','block');
+		else
+			container.children('.server-flyout-content').css('display','none');
+	});
+
+	$("body").click
+    (
+        function(e)
+        {
+            if(!$(e.target).closest(".server-dropdown").length)
+            {
+				$('.server-dropdown').removeClass('js-flyout');
+				$('.server-flyout-content').css('display','none');
+            }
+        }
+    );
 });
